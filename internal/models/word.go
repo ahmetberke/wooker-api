0db7ef0ab9fefcc79363f8de470395592ae6a10f
ahmetberke <tuncel.ahmet.berke@gmail.com>
2022-02-09 21:39:55 +0300

    added word model

--- internal/models/word.go
+++ internal/models/word.go
@@ -1,1 +1,9 @@
 package models
+
+type Word struct {
+	Name string `json:"name"`
+	Mean string `json:"mean"`
+	Story string `json:"story"`
+	UserID uint `json:"user_id"`
+	User *User `json:"user"`
+}
